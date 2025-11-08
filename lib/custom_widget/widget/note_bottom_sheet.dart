4acf578ca06e4d571f8f36271ac276f4526b253d
???? ??? <????? ?????????? ???>
2025-11-09 00:17:03 +0200

    Finish Bootom Sheet& Edit Note View

--- lib/custom_widget/widget/note_bottom_sheet.dart
+++ lib/custom_widget/widget/note_bottom_sheet.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:notes_app3/custom_widget/custom_bottom.dart';
 import 'package:notes_app3/custom_widget/custom_text_field.dart';
 
 class AddNoteBttomSheet extends StatelessWidget {
@@ -9,11 +10,16 @@ class AddNoteBttomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12),
-      child: const Column(
-        children: [
-          SizedBox(height: 32,),
-          CustomTextField(hint: 'hajer',),
-        ],
+      child: SingleChildScrollView(
+        child: const Column(
+          children: [
+            SizedBox(height: 32,),
+            CustomTextField(hint: 'title',),SizedBox(height: 20,),CustomTextField(hint: "content",maxLine: 6,),
+            SizedBox(height: 80,),
+            CustomBottom(),SizedBox(height: 18,)
+        
+          ],
+        ),
       ),
     );
   }
