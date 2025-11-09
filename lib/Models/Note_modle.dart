c35358759ce2dc74fc92d4e915ca158a8cf89f04
???? ??? <????? ?????????? ???>
2025-11-09 14:36:55 +0200

    Create NoteModle

---
+++ lib/Models/Note_modle.dart
@@ -0,0 +1,14 @@
+
+
+class NoteModle{
+
+  final String title;
+
+  final String subTitle;
+
+  final String date;
+  final int color;
+
+  NoteModle({required this.title, required this.subTitle, required this.date, required this.color});
+
+}
