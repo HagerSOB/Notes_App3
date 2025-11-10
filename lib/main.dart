162a2b0bbfe392edb8b06d612f42c5810172d334
???? ??? <????? ?????????? ???>
2025-11-10 20:21:57 +0200

    init Hive AndOpenBox

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:notes_app3/constants.dart';
 import 'package:notes_app3/views/notes_view.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+ await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
 class NotesApp extends StatelessWidget {
