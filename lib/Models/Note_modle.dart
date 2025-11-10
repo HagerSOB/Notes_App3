10d9746a704cfd0c805418d661d44a2382c83cae
???? ??? <????? ?????????? ???>
2025-11-10 20:14:20 +0200

    create type adaptor for notemodle

--- lib/Models/Note_modle.dart
+++ lib/Models/Note_modle.dart
@@ -1,12 +1,20 @@
 
 
-class NoteModle{
+import 'package:hive/hive.dart';
+part'Note_modle.g.dart';
 
+@HiveType(typeId: 0)
+class NoteModle extends HiveObject{
+  @HiveField(0)
   final String title;
+  @HiveField(1)
 
   final String subTitle;
+  @HiveField(2)
 
   final String date;
+  @HiveField(3)
+
   final int color;
 
   NoteModle({required this.title, required this.subTitle, required this.date, required this.color});
