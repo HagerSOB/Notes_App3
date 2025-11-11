20876f635c9daae93693ccedbf6385c80a0f14ee
???? ??? <????? ?????????? ???>
2025-11-11 03:23:32 +0200

    Validate User Input

---
+++ lib/custom_widget/widget/add_note_form.dart
@@ -0,0 +1,64 @@
+
+import 'package:flutter/material.dart';
+import 'package:notes_app3/custom_widget/custom_bottom.dart';
+import 'package:notes_app3/custom_widget/custom_text_field.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> FormKey=GlobalKey();
+  AutovalidateMode autovalidateMode=AutovalidateMode.disabled;
+  String ? title,subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: FormKey,
+      child:  Column(
+        children: [
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextField(
+            onSaved: (value){
+              title=value;
+            },
+            hint: 'title',
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          CustomTextField(
+            onSaved: (value){
+              subTitle=value;
+            },
+
+            hint: "content",
+            maxLine: 6,
+          ),
+          SizedBox(
+            height: 80,
+          ),
+          CustomBottom(
+              onTap: () {
+                if (FormKey.currentState!.validate()) {
+                  FormKey.currentState!.save();
+                } else {
+                  autovalidateMode = AutovalidateMode.always;
+                  setState(() {});
+                }
+              }          ),
+          SizedBox(
+            height: 18,
+          )
+        ],
+      ),
+    );
+  }
+}
