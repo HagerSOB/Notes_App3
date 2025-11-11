051a42f5925ef41cfea54fc524548405254ac74b
???? ??? <????? ?????????? ???>
2025-11-12 00:04:21 +0200

    RegisterAdapter

--- lib/cuibts/add_note_cubit.dart
+++ lib/cuibts/add_note_cubit.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app3/Models/Note_modle.dart';
 part 'add_note_state.dart';
 class NotesCubit extends Cubit<NotesState>{
   NotesCubit():super (NotesInitial());
+ addNote( NoteModle note){}
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:notes_app3/Models/Note_modle.dart';
 import 'package:notes_app3/constants.dart';
 import 'package:notes_app3/views/notes_view.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async{
   await Hive.initFlutter();
+  Hive.registerAdapter(NoteModleAdapter());
  await Hive.openBox(kNotesBox);
   runApp(const NotesApp());
 }
